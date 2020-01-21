d93a7284aea566a0e08a65e9cf9ecbaf14d01c0d
venkykolikonda <56423133+venkykolikonda@users.noreply.github.com>
2020-01-21 12:45:42 +0530

    venky add files

---
+++ eventbinding-JS/largest.js
@@ -0,0 +1,9 @@
+var a=[21,46,22,48,220,488,550,196];
+var largest=0;
+for(var i=0;i<=a.length;i++)
+{
+  if(a[i]>largest){
+largest=a[i];  
+}
+}
+console.log(largest);
---
+++ eventbinding-JS/pyramid.js
@@ -0,0 +1,7 @@
+for(var i=1;i<=5;i++){
+    var a="";
+    for( var j=1;j<=i;j++){
+        a=a+" "+ j;
+    }
+    console.log(a)
+}
